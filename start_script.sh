95ec85a62bc3f86044f6452be8d9c2c0e1b78500
ahmadrozaq <94879061+ahmadrozaq@users.noreply.github.com>
2025-01-08 20:17:40 +0700

    Update start_script.sh

--- start_script.sh
+++ start_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Memulai proses mining
 #/path/to/miner --options &
-/home/user/Downloads/ccminer-verus/main.sh  --options &
+#/home/user/Downloads/ccminer-verus/main.sh  --options &
+/home/user/Downloads/tahan/main.sh  --options &
 echo $! > /tmp/ccminer
